b9bcb2db34e445123bb255c98f31b8e24becb12c
SANJAY222-2006 <sanjay236002@gmail.com>
2025-07-05 20:15:07 +0530

    majorchange1

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 import express from "express";
-import {PORT} from "./config/env.js";
+import cookieParser from "cookie-parser";
+import { PORT } from "./config/env.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
-const app=express();
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
+const app = express();
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use(errorMiddleware);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.listen(PORT,async ()=>{
-    console.log(`server listening on port ${PORT}...`)
-    await connectToDatabase()
+app.listen(PORT, async () => {
+  console.log(`server listening on port ${PORT}...`);
+  await connectToDatabase();
 });
 
 export default app;
