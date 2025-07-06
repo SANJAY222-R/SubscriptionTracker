ff6c2afc8d0f36ed212b7f831250a0101078ce49
SANJAY222-2006 <sanjay236002@gmail.com>
2025-07-06 22:38:13 +0530

    jj

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,19 +1,20 @@
-import {Router} from "express";
+import { Router } from "express";
+import { getUsers, getUser } from "../controllers/user.controller";
+import authorize from "../middleware/auth.middleware.js";
 const userRouter = Router();
-userRouter.get("/", (req, res) => {
-    res.send({title:"Get all users"})
-});
-userRouter.get("/:id", (req, res) => {
-    res.send({title:"Get a user by id"})
-});
+
+userRouter.get("/", getUsers);
+
+userRouter.get("/:id", authorize, getUser);
+
 userRouter.post("/", (req, res) => {
-    res.send({title:"create a new user"})
+  res.send({ title: "create a new user" });
 });
 userRouter.put("/:id", (req, res) => {
-    res.send({title:"update a user"})
+  res.send({ title: "update a user" });
 });
 userRouter.delete("/:id", (req, res) => {
-    res.send({title:"delete a user"})
+  res.send({ title: "delete a user" });
 });
 
 export default userRouter;
